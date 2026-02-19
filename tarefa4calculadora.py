8150811a4bb29f6dcf5506d2ea815257bb6f2765
Paula Alonso <mjsafr951753@gmail.com>
2026-02-19 12:26:02 +0100

    documentando tarefa4calculadora

--- tarefa4calculadora.py
+++ tarefa4calculadora.py
@@ -1,4 +1,7 @@
-
+# Calculadora Simple en Python
+# Este es un programa sencillo de calculadora que permite al usuario 
+# realizar operaciones matemáticas básicas: suma, resta y multiplicación.
+# Funciones de la calculadora
 
 def sumar(a, b):
     return a + b
@@ -6,11 +9,11 @@ def sumar(a, b):
 def restar(a, b):
     return a - b
 
-def multiplicar(a, b):
-    return a + b
+def multiplicar(a, b): 
+    return a * b 
 
-print("Calculadora simple")
-num1 = float(input("Introduce o primero número: "))
+print("Calculadora simple") 
+num1 = float(input("Introduce o primero número: ")) # 
 num2 = float(input("Introduce o segundo número: "))
 print("Suma:", sumar(num1, num2))
 print("Resta:", restar(num1, num2)) 
